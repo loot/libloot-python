1fb65022939f811ffbc614f88a367f9990b26a04
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-11-06 15:10:57 +0000

    Increment version to 1.1.1

--- src/test.py
+++ src/test.py
@@ -45,10 +45,10 @@ class TestLootApi(GameFixture):
     def test_wrapper_version(self):
         self.assertEqual(WrapperVersion.major, 1)
         self.assertEqual(WrapperVersion.minor, 1)
-        self.assertEqual(WrapperVersion.patch, 0)
+        self.assertEqual(WrapperVersion.patch, 1)
         self.assertNotEqual(WrapperVersion.revision, u'')
         self.assertNotEqual(WrapperVersion.revision, Version.revision)
-        self.assertEqual(WrapperVersion.string(), "1.1.0")
+        self.assertEqual(WrapperVersion.string(), "1.1.1")
 
     def test_create_db(self):
         db = create_database(GameType.tes4, self.game_path, self.local_path)
